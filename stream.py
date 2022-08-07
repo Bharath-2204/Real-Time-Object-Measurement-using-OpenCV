f8735af14b022199e85d98e7bb6f4c6628db23b8
Bharath <bharathbdh65@gmail.com>
2022-08-08 01:06:54 +0530

    Update stream.py

--- stream.py
+++ stream.py
@@ -1,8 +1,17 @@
-
+import numpy as np
+import cv2
 import streamlit as st
-from annotated_text import annotated_text
+from PIL import Image
+import imutils
+import scipy.spatial as sp
+from imutils import contours
+from imutils import perspective
+from scipy.spatial import distance as dist
 from final_rtom import *
+from badacc import *
+from annotated_text import annotated_text
 
+        
 
 st.title("Real Time Object Measurement")
 annotated_text(("open","CV","#269e98"),("with","python","#d16c06"),)
@@ -11,50 +20,86 @@ st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}', unsafe_allo
 
 options = st.sidebar.radio("Choose",('Real','Static'))
 if options == 'Real':
-    st.subheader("This app allows you to measure objects in real time with your webcam!")
-    st.text("We use OpenCV and Streamlit for this demo")
-    
-    with st.sidebar:
-        with st.expander("INSTRUCTIONS TO USE IP WEBCAM"):
-            #st.sidebar.title("INSTRUCTIONS TO USE IPWEBCAM")
-                st.write("1.Download the IP Webcam App from PlayStore on your Mobile")
-                st.write("2.Open the App")
-                st.write("3.Scroll down and click on Start Server. Your Mobile's Camera will be turned on")
-                st.write("4.Notedown the IP Address displayed")
-             
-        with st.expander("WHAT IS AN ARUCO MARKER?"):
-            st.write("""
-                An aruco marker is a fiducial marker that is placed on the object or scene being imaged. 
-                It is a binary square with black background and boundaries and a white generated pattern within it that uniquely identifies it.
-                The black boundary helps making their detection easier. They can be generated in a variety of sizes.
-                The idea is that you print these markers and put them in the real world.
-                You can photograph the real world and detect these markers uniquely.
-            """)
-            st.warning("Real time measurement won't workout without arUco marker")
-            st.info('Please do find the instructions and link to download the arUco marker below ')
+    try:
+        st.subheader("This app allows you to measure objects in real time with your webcam!")
+        st.text("We use OpenCV and Streamlit for this demo")
         
-        with st.expander("INSTRUCTIONS TO USE ARUCO"): 
-            #st.sidebar.title("INSTRUCTIONS TO USE ARUCO")
+        with st.sidebar:
+            with st.expander("INSTRUCTIONS TO USE IP WEBCAM"):
+                #st.sidebar.title("INSTRUCTIONS TO USE IPWEBCAM")
+                    st.write("1.Download the IP Webcam App from PlayStore on your Mobile")
+                    st.write("2.Open the App")
+                    st.write("3.Scroll down and click on Start Server. Your Mobile's Camera will be turned on")
+                    st.write("4.Notedown the IP Address displayed")
+                
+            with st.expander("WHAT IS AN ARUCO MARKER?"):
+                st.write("""
+                    An aruco marker is a fiducial marker that is placed on the object or scene being imaged. 
+                    It is a binary square with black background and boundaries and a white generated pattern within it that uniquely identifies it.
+                    The black boundary helps making their detection easier. They can be generated in a variety of sizes.
+                    The idea is that you print these markers and put them in the real world.
+                    You can photograph the real world and detect these markers uniquely.
+                """)
+                st.warning("Real time measurement won't workout without arUco marker")
+                st.info('Please do find the instructions and link to download the arUco marker below ')
             
-            st.write("1.Download the aruco marker PDF")
-            st.write("2.Print the PDF")
-            st.write("3.Place object to be measured beside arUco marker")
-            st.write("4.Enter IP address")
-            st.write("5.Point webcam such that arUco marker and object are in the same frame")
-            st.write("6.Enjoy the real-time accuracy !!")
-            
-            
-            st.sidebar.subheader("Download ArucoMarker")
-            with open("ar6.pdf", "rb") as pdf_file:
-                PDFbyte = pdf_file.read()
+            with st.expander("INSTRUCTIONS TO USE ARUCO"): 
+                #st.sidebar.title("INSTRUCTIONS TO USE ARUCO")
+                
+                st.write("1.Download the aruco marker PDF")
+                st.write("2.Print the PDF")
+                st.write("3.Place object to be measured beside arUco marker")
+                st.write("4.Enter IP address")
+                st.write("5.Point webcam such that arUco marker and object are in the same frame")
+                st.write("6.Enjoy the real-time accuracy !!")
+                
+                
+                st.sidebar.subheader("Download ArucoMarker")
+                with open("ar6.pdf", "rb") as pdf_file:
+                    PDFbyte = pdf_file.read()
 
-            st.sidebar.download_button(label="Aruco_Marker",
-                                data=PDFbyte,
-                                file_name="Aruco_marker.pdf",
-                                mime='application/octet-stream')
-        st.markdown('##')
-    live()
-    
+                st.sidebar.download_button(label="Aruco_Marker",
+                                    data=PDFbyte,
+                                    file_name="Aruco_marker.pdf",
+                                    mime='application/octet-stream')
+            st.markdown('##')
+            
+        
+        op = st.radio("Choose the Webcam",('IP WEBCAM','External Webcam'))
+        if op == 'IP WEBCAM':
+                user_input = st.text_input("Enter the URL of your IP Webcam:")
+                vid = cv2.VideoCapture(user_input)
+                st.markdown("###### URL format : http://your_ip_address:8080/video")
+                st.markdown("###### Example URL : http://192.168.25.243:8080/video")
+        elif op == 'External Webcam':
+                vid = cv2.VideoCapture(0)
+        opt = st.radio("",('Start','Pause'))
+            #sto = st.button("Stop")
+            
+        st.write("##### Choose the shape of the Object you want to measure")    
+        col1, col2 = st.columns([1,3])
+        with col1:
+                check1 = st.button("Rectangle/Square")
+        with col2:
+                check2 = st.button("Circle")
+        like = st.slider('How satisfied are you with the measurements?', 0, 10, 0)
+        if like >=5:
+                st.balloons()
+        FRAME_WINDOW = st.image([]) 
+        
+            
+        if opt == 'Start':
+                while True:    
+                    _, ima = vid.read()
+                    FRAME_WINDOW.image(ima)                         #Displaying the live video
+                    if check1:                  
+                            getcontours(ima,showCanny=True) 
+                    elif check2:
+                            getcircles(ima)
+                    if opt == 'Stop':
+                        break 
+    except:
+        pass
 elif options == 'Static':
    
     st.subheader("This app also allows you to measure static objects !")
@@ -70,9 +115,15 @@ elif options == 'Static':
         if satisfied:
             st.snow()
             st.write('Thanks :)')
-    try:        
+    try:
         static()
     except:
         pass
-    
-
+st.write('<style>.css-1yjuwjr{font-size:22px;}', unsafe_allow_html=True)
+st.write('<style>.st-af{font-size:1.2rem;}', unsafe_allow_html=True)
+st.write('<style>.st-cw{height:1.6rem;}', unsafe_allow_html=True)
+st.write('<style>.st-d2{width:1.6rem;}', unsafe_allow_html=True)
+st.write('<style>.st-d6{height:8px;}', unsafe_allow_html=True)
+st.write('<style>.st-d7{width:8px;}', unsafe_allow_html=True)
+st.write('<style>code{font-size:1.1em;}', unsafe_allow_html=True)
+st.write('<style>p{font-size:1.2rem;}', unsafe_allow_html=True)
