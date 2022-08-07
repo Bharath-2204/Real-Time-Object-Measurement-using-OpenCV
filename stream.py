1f9e30e018d6990163b3aa9c02ccb94ec010db61
Bharath <bharathbdh65@gmail.com>
2022-08-08 00:03:43 +0530

    Update stream.py

--- stream.py
+++ stream.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from annotated_text import annotated_text
 from final_rtom import *
+from badacc import *
 
 st.title("Real Time Object Measurement")
 annotated_text(("open","CV","#269e98"),("with","python","#d16c06"),)
